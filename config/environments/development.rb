16c898f3faba733acd4e6070f3a37b26433dc657
vlad <svuitis@gmail.com>
2015-06-03 17:48:40 +0300

    editing gitignore

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
       :port                 => 587,
       :domain               => "gmail.com",
       :user_name            => "<login>",
-      :password             => "<passwor>",
+      :password             => "<password>",
       :authentication       => "plain",
       :enable_starttls_auto => true
   }
