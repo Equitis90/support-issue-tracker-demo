e78b7bd16f99f36aef192f867baef3c4099a526f
Equitis90 <svuitis@gmail.com>
2015-06-03 17:29:19 +0300

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,8 +44,8 @@ Rails.application.configure do
       :address              => "smtp.gmail.com",
       :port                 => 587,
       :domain               => "gmail.com",
-      :user_name            => "ttest7249@gmail.com",
-      :password             => "test_pass",
+      :user_name            => <login>,
+      :password             => <password>,
       :authentication       => "plain",
       :enable_starttls_auto => true
   }
