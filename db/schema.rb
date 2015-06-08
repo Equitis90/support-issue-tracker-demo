99ee2331f8ddd07989466bbfced88698e3e38e77
Владислав <svuitis@gmail.com>
2015-06-08 21:05:20 +0300

    tabs completing

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20150528130728) do
   enable_extension "plpgsql"
 
   create_table "departments", force: true do |t|
-    t.string   "title", null: false
+    t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -46,13 +46,16 @@ ActiveRecord::Schema.define(version: 20150528130728) do
     t.string   "creator_email",    null: false
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: true do |t|
     t.string   "login",                                   null: false
     t.string   "encrypted_password"
+    t.string   "encrypted_password_salt"
+    t.string   "encrypted_password_iv"
     t.string   "username",                default: "",    null: false
     t.integer  "department_id"
     t.boolean  "admin",                   default: false
     t.datetime "created_at",                              null: false
     t.datetime "updated_at",                              null: false
   end
+
 end
